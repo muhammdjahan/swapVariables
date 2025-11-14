4739cd13bd41f2fb150b0e91a8527af1d95354ec
muhammadjahan <muhammadreza.jahanshahi@gmail.com>
2025-11-14 09:50:44 +0330

    swap two number by third variable

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+we want swap two number by third variable
+1: define first number 
+2: define secound number
+3: define third variable and that value equal to first number.
+
+then we might to first value equal to secound value
+finaly, secound value equal to first number.
---
+++ SwapTwoNum.py
@@ -0,0 +1,9 @@
+# swap two variable by third variable
+
+firstNumber = input("enter first number: ")
+secoundNumber = input("enter secound number: ")
+c=firstNumber
+firstNumber=secoundNumber
+secoundNumber=c
+
+print(f"first number is {firstNumber} and secound number is {secoundNumber} ")
